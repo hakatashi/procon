a8a801cdef6bb38ed04e63053427449a36a07710
Koki Takahashi <hakatasiloving@gmail.com>
2019-07-15 00:32:24 +0900

    Update crystal.sh

--- crystal.sh
+++ crystal.sh
@@ -1,5 +1,5 @@
 if [ "$1" -nt a.out ]; then
-	/usr/local/share/crystal-0.20.5-1/bin/crystal build "$1" -o a.out || exit
+	/usr/local/share/crystal-0.20.5-1/bin/crystal build "$1" -o a.out --error-trace || exit
 fi
 if [ ! -f in.txt ]; then
 	touch in.txt
