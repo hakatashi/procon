9e33a3aca3b2b34c286c707f3627fa28d5831240
Koki Takahashi <hakatasiloving@gmail.com>
2019-09-23 20:38:09 +0900

    Rewrite abc141_d with new implementation of PriorityQueue

--- abc141_d.cr
+++ abc141_d.cr
@@ -1,79 +1,64 @@
+# PriorityQueue.cr by Koki Takahashi
+# Licensed under MIT License
+
 class PriorityQueue(T)
-  def initialize(capacity : Int32)
-    @elem = Array(T).new(capacity)
-  end
- 
-  def initialize(list : Enumerable(T))
-    @elem = list.to_a
-    1.upto(size - 1) { |i| fixup(i) }
-  end
- 
-  def size
-    @elem.size
+  property heap : Array(T)
+
+  def initialize
+    @heap = Array(T).new
   end
 
-  def <<(value : T)
-    push(value)
-  end
- 
-  def push(v)
-    @elem << v
-    fixup(size - 1)
-  end
- 
-  def top
-    @elem[0]
-  end
- 
-  def pop
-    ret = @elem[0]
-    last = @elem.pop
-    if size > 0
-      @elem[0] = last
-      fixdown(0)
-    end
-    ret
-  end
- 
-  def decrease_top(new_value : T)
-    @elem[0] = new_value
-    fixdown(0)
-  end
- 
-  def to_s(io : IO)
-    io << @elem
-  end
- 
-  private def fixup(index : Int32)
-    while index > 0
+  def push(v : T)
+    @heap << v
+    index = @heap.size - 1
+    while index != 0
       parent = (index - 1) / 2
-      break if @elem[parent] >= @elem[index]
-      @elem[parent], @elem[index] = @elem[index], @elem[parent]
+      if @heap[parent] >= @heap[index]
+        break
+      end
+      @heap[parent], @heap[index] = @heap[index], @heap[parent]
       index = parent
     end
   end
- 
-  private def fixdown(index : Int32)
-    while true
-      left = index * 2 + 1
-      break if left >= size
-      right = index * 2 + 2
-      child = right >= size || @elem[left] > @elem[right] ? left : right
-      if @elem[child] > @elem[index]
-        @elem[child], @elem[index] = @elem[index], @elem[child]
-        index = child
+
+  def <<(v : T)
+    push(v)
+  end
+
+  def pop
+    if @heap.size == 0
+      return nil
+    end
+    if @heap.size == 1
+      return @heap.pop
+    end
+    ret = @heap.first
+    @heap[0] = @heap.pop
+    index = 0
+    while index * 2 + 1 < @heap.size
+      child = if index * 2 + 2 < @heap.size && @heap[index * 2 + 1] < @heap[index * 2 + 2]
+        index * 2 + 2
       else
+        index * 2 + 1
+      end
+      if @heap[index] >= @heap[child]
         break
       end
+      @heap[child], @heap[index] = @heap[index], @heap[child]
+      index = child
     end
+    ret
   end
+
+  delegate :empty?, to: @heap
 end
+
 
 
 n, m = read_line.split.map(&.to_u64)
 ais = read_line.split.map(&.to_u64)
 
-q = PriorityQueue(UInt64).new(n.to_i32)
+q = PriorityQueue(UInt64).new
 ais.each {|a| q << a}
 
 m.times do
