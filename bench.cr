f24faf832c076d976094fd35f01d56e54f77c16a
Koki Takahashi <hakatasiloving@gmail.com>
2019-10-31 16:27:58 +0900

    Add bench and test to UnionFindTree

--- bench.cr
+++ bench.cr
@@ -1,3 +1,4 @@
 require "./library/PriorityQueue.bench.cr"
 require "./library/SegmentTree.bench.cr"
 require "./library/RedBlackTree.bench.cr"
+require "./library/UnionFindTree.bench.cr"
--- test.cr
+++ test.cr
@@ -1,3 +1,4 @@
 require "./library/PriorityQueue.test.cr"
 require "./library/SegmentTree.test.cr"
 require "./library/RedBlackTree.test.cr"
+require "./library/UnionFindTree.test.cr"
