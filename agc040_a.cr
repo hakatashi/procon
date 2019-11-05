2db5d988d940a9e86e6e2c14b640458cc4499283
Koki Takahashi <hakatasiloving@gmail.com>
2019-11-05 23:50:28 +0900

    Add agc040_a.cr

---
+++ agc040_a.cr
@@ -0,0 +1,37 @@
+s = read_line
+up = 0_u64
+down = 0_u64
+if s[0] == '>'
+  down = 1_u64
+else
+  up = 1_u64
+end
+ret = 0_u64
+s.chars.each_cons(2) do |(pc, c)|
+  if pc == '>' && c == '<'
+    if up == down
+      ret += up * up
+    else
+      max = [up, down].max
+      min = [up, down].min
+      ret += max * (max + 1) / 2
+      ret += min * (min - 1) / 2
+    end
+    up = 0_u64
+    down = 0_u64
+  end
+  if c == '>'
+    down += 1_u64
+  else
+    up += 1_u64
+  end
+end
+if up == down
+  ret += up * up
+else
+  max = [up, down].max
+  min = [up, down].min
+  ret += max * (max + 1) / 2
+  ret += min * (min - 1) / 2
+end
+p ret
