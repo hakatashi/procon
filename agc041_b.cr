926296d5c0281970b0e7e11ad3f35bc987f465a7
Koki Takahashi <hakatasiloving@gmail.com>
2019-12-29 19:56:45 +0900

    Add files for agc041

---
+++ agc041_b.cr
@@ -0,0 +1,25 @@
+n, m, v, pe = read_line.split.map(&.to_i64)
+ais = read_line.split.map(&.to_i64).sort.reverse
+sums = [0]
+rsums = [0]
+ais.each do |a|
+  sums << sums.last + a
+end
+ais.reverse.each do |a|
+  rsums << rsums.last + a
+end
+ans = ais.bsearch_index do |a, i|
+  if i < pe - 1
+    false
+  elsif a + m <= ais[pe - 1]
+    true
+  else
+    s = ais.bsearch_index {|a2| a2 <= a + m}
+    if s.nil? || s < pe - 1
+      (pe - 1) * m + (i - (pe - 1)) * (a + m) - (sums[i] - sums[pe - 1]) + (n - i) * m <= m * v
+    else
+      (pe - 1) * m + (i - s) * (a + m) - (sums[i] - sums[s]) + (n - i) * m <= m * v
+    end
+  end
+end
+p ans
---
+++ agc041_c.cr
@@ -0,0 +1,84 @@
+boards = [
+  [] of String,
+  [] of String,
+  [] of String,
+  [
+    "acc",
+    "a.d",
+    "bbd",
+  ],
+  [
+    "abcc",
+    "abdd",
+    "eegh",
+    "ffgh",
+  ],
+  [
+    "abbcc",
+    "ad..e",
+    "fd..e",
+    "f.ggh",
+    "iijjh",
+  ],
+  [
+    "acca..",
+    "a.da..",
+    "bbd.bb",
+    "cc.acc",
+    "..da.d",
+    "..dbbd",
+  ],
+  [
+    "aabbcc.",
+    "ddee..f",
+    "gghh..f",
+    "....ijk",
+    "....ijk",
+    "....lmn",
+    "....lmn",
+  ],
+  [
+    "abcc....",
+    "abdd....",
+    "eegh....",
+    "ffgh....",
+    "....abcc",
+    "....abdd",
+    "....eegh",
+    "....ffgh",
+  ],
+  [
+    "acca.....",
+    "a.da.....",
+    "bbd.bb...",
+    "...acca..",
+    "...a.da..",
+    "...bbd.bb",
+    "a.....acc",
+    "a.....a.d",
+    ".bb...bbd",
+  ],
+]
+
+n = read_line.to_i
+if n == 2
+  p -1
+  exit
+end
+
+if n == 3 || n == 4 || n == 5
+  n.times do |i|
+    puts boards[n][i]
+  end
+  exit
+end
+
+step_size = (n - 6) / 4 * 4
+n.times do |i|
+  if i < step_size
+    step = i / 4
+    puts "." * (step * 4) + boards[4][i % 4] + "." * (n - (step + 1) * 4)
+  else
+    puts "." * step_size + boards[n - step_size][i - step_size]
+  end
+end
