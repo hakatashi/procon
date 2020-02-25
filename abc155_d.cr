20083adcc8fc504a2c592af45fa4523be63995fa
Koki Takahashi <hakatasiloving@gmail.com>
2020-02-26 04:59:07 +0900

    Fix abc155_d implementation

--- abc155_d.cr
+++ abc155_d.cr
@@ -1,33 +1,63 @@
-def max(a, b)
-  a > b ? a : b
-end
-def min(a, b)
-  a < b ? a : b
-end
-
 n, k = read_line.split.map(&.to_i64)
-ais = read_line.split.map(&.to_i64).sort
-rev_ais = ais.reverse
+zeros = Array(Int64).new(n)
+positives = Array(Int64).new(n)
+negatives = Array(Int64).new(n)
+read_line.split.map do |ra|
+  a = ra.to_i64
+  if a == 0
+    zeros << a
+  elsif a < 0
+    negatives << a
+  else
+    positives << a
+  end
+end
+positives.sort!
+negatives.sort_by! {|a| a.abs}
+
+# iより小さいかiと等しい個数がk個より多いかk個ならtrueを返すこと
 ans = (-1_000_000_000_000_000_000_i64..1_000_000_000_000_000_000_i64).bsearch do |i|
+  # iより小さいか等しい個数
   ret = 0_i64
-  ais.each_with_index do |a, j|
-    if a < 0
-      i2 = rev_ais.bsearch_index {|a2| a2 * a >= i}
-      unless i2.nil?
-        ret += min(n - i2 - j - 1, n - j - 1)
+  if i > 0
+    ptr = positives.size.to_i64 - 1 # positives[ptr] * a <= i となる最小のインデックス
+    positives.each_with_index do |a, j|
+      until ptr < 0 || positives[ptr] * a <= i
+        ptr -= 1
       end
-    elsif a == 0
-      if i <= 0
-        ret += n - j - 1
-      end
-    else
-      i2 = ais.bsearch_index {|a2| a2 * a >= i}
-      unless i2.nil?
-        ret += min(n - i2 - 1, n - j - 1)
+      if a * a <= i
+        # 自分自身を除く
+        ret += ptr
+      else
+        ret += ptr + 1
       end
     end
+    ptr = negatives.size.to_i64 - 1
+    negatives.each_with_index do |a, j|
+      until ptr < 0 || negatives[ptr] * a <= i
+        ptr -= 1
+      end
+      if a * a <= i
+        # 自分自身を除く
+        ret += ptr
+      else
+        ret += ptr + 1
+      end
+    end
+    ret += zeros.size.to_i64 * (n - zeros.size) * 2 + zeros.size.to_i64 * (zeros.size - 1)
+    ret += positives.size.to_i64 * negatives.size * 2
+  elsif i == 0
+    ret += zeros.size.to_i64 * (n - zeros.size) * 2 + zeros.size.to_i64 * (zeros.size - 1)
+    ret += positives.size.to_i64 * negatives.size * 2
+  else
+    ptr = negatives.size.to_i64 - 1 # negatives[ptr] * a > i となる最大のインデックス
+    positives.each_with_index do |a, j|
+      until ptr < 0 || negatives[ptr] * a > i
+        ptr -= 1
+      end
+      ret += (negatives.size - 1 - ptr) * 2
+    end
   end
-  p [i, ret]
-  ret >= k
+  ret / 2 >= k
 end
 p ans
