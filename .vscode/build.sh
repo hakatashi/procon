0d0e6edf4c3bded93905e6b2d26503aeb7a71c5d
Koki Takahashi <hakatasiloving@gmail.com>
2020-04-05 12:21:35 +0900

    Update Crystal version from 0.20.5 to 0.33.0

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -10,15 +10,15 @@ if [ "${filename##*.}" = "cpp" ]; then
 elif [ "${filename##*.}" = "cr" ]; then
   if [ "$filename" -nt "${output}" ]; then
     echo "Building Crystal project ${filename}..."
-    if [ -d /usr/local/share/crystal-0.20.5-1 ]; then
+    if [ -d /usr/local/share/crystal-0.33.0-1 ]; then
       mkdir -p bin
-      /usr/local/share/crystal-0.20.5-1/bin/crystal build "$filename" -o "${output}" --error-trace || exit 1
-    elif [ -d /tmp/crystal-0.20.5-1 ]; then
+      /usr/local/share/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace || exit 1
+    elif [ -d /tmp/crystal-0.33.0-1 ]; then
       mkdir -p bin
-      /tmp/crystal-0.20.5-1/bin/crystal build "$filename" -o "${output}" --error-trace || exit 1
+      /tmp/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace || exit 1
     else
       mkdir -p /mnt/bin
-      docker run -v "$PWD":/mnt crystallang/crystal:0.20.5 crystal build "/mnt/$filename" -o "/mnt/${output}" --error-trace || exit 1
+      docker run -v "$PWD":/mnt crystallang/crystal:0.33.0 crystal build "/mnt/$filename" -o "/mnt/${output}" --error-trace || exit 1
     fi
   fi
 elif [ "${filename##*.}" = "vim" ]; then
