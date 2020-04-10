1a8fe1882913b42153179302a809a2514f201275
Koki Takahashi <hakatasiloving@gmail.com>
2020-04-10 10:32:38 +0900

    Make build for oj-verify release mode

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -1,34 +1,34 @@
-filename="$1"
-if [ "$#" = 1 ]; then
+filename="$1"; shift
+if [ "$#" = 0 ] || [[ "$1" = -* ]]; then
   output="bin/${filename%.*}"
 else
-  output="$2"
+  output="$1"; shift
 fi
 
 if [ "${filename##*.}" = "cpp" ]; then
   if [ "$filename" -nt "${output}" ]; then
     echo "Building C++ project ${filename}..."
     mkdir -p bin
-    g++ "$filename" -O2 -lm -o "${output}"
+    g++ "$filename" -O2 -lm -o "${output}" $@
   fi
 elif [ "${filename##*.}" = "cr" ]; then
   if [ "$filename" -nt "${output}" ]; then
     echo "Building Crystal project ${filename}..."
     if [ -d /usr/local/share/crystal-0.33.0-1 ]; then
       mkdir -p bin
-      /usr/local/share/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace || exit 1
+      /usr/local/share/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
     elif [ -d /tmp/crystal-0.33.0-1 ]; then
       mkdir -p bin
-      /tmp/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace || exit 1
+      /tmp/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
     else
       mkdir -p /mnt/bin
-      docker run -v "$PWD":/mnt crystallang/crystal:0.33.0 crystal build "/mnt/$filename" -o "/mnt/${output}" --error-trace || exit 1
+      docker run -v "$PWD":/mnt crystallang/crystal:0.33.0 crystal build "/mnt/$filename" -o "/mnt/${output}" --error-trace $@ || exit 1
     fi
   fi
 elif [ "${filename##*.}" = "vim" ]; then
   if [ "$filename" -nt "${output}" ]; then
     echo "Building Vim project ${filename}..."
-    ruby library/vim-compiler.rb "$filename" "${output}"
+    ruby library/vim-compiler.rb "$filename" "${output}" $@
   fi
 fi
 
